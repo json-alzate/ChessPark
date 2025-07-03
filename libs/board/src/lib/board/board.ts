9ba198fc67b20c6dd996a6fbcab2d13226f2fb3a
Json Alzate <developer.alzate@gmail.com>
2025-07-02 19:27:23 -0500

    🏗️ Board lib structure

---
+++ libs/board/src/lib/board/board.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-board',
+  imports: [CommonModule],
+  templateUrl: './board.html',
+  styleUrl: './board.css',
+})
+export class Board {}
