6d965a23b4d7b93843a71409bfcf292cece7a1fb
Json Alzate <developer.alzate@gmail.com>
2025-07-07 11:16:08 -0500

    🏗️ state lib architecture

---
+++ libs/state/src/lib/state/state.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-state',
+  imports: [CommonModule],
+  templateUrl: './state.html',
+  styleUrl: './state.css',
+})
+export class State {}
