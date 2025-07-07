e60e11e2b1d91a41ec0c6883db5a2c296dc828c0
Json Alzate <developer.alzate@gmail.com>
2025-07-07 11:17:42 -0500

    🏗️ widgets lib structure

---
+++ libs/widgets/src/lib/widgets/widgets.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-widgets',
+  imports: [CommonModule],
+  templateUrl: './widgets.html',
+  styleUrl: './widgets.css',
+})
+export class Widgets {}
