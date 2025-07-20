436d3093103773a273e3f672ea6452993f5b1d00
Json Alzate <developer.alzate@gmail.com>
2025-07-20 11:37:36 -0500

    ✨ Actualización de la configuración de nx.json y package.json para incluir nuevas dependencias de Capacitor y Ionic, creacion de app demoB

---
+++ apps/demoB/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'demoB',
+  webDir: '../../dist/apps/demoB/browser'
+};
+
+export default config;
