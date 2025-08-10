cd3d82dac92b5aac878ca912b772e5c97aef7327
Json Alzate <developer.alzate@gmail.com>
2025-08-10 14:22:27 -0500

    ✨ Mejora en la interfaz de usuario del componente de coordenadas, incluyendo la reestructuración del HTML y la adición de estilos CSS para el tablero y elementos visuales. Implementación de lógica para la generación de puzzles y temporizador en el componente TypeScript.

--- apps/chessCoordinatesTrainer/src/app/pages/coordinates/coordinates.page.ts
+++ apps/chessCoordinatesTrainer/src/app/pages/coordinates/coordinates.page.ts
@@ -8,6 +8,7 @@ import {   IonContent } from '@ionic/angular/standalone';
 
 
 import { BoardComponent } from '@chesspark/board';
+import { interval, Observable, Subject, takeUntil } from 'rxjs';
 
 
 @Component({
@@ -18,6 +19,145 @@ import { BoardComponent } from '@chesspark/board';
   imports: [ CommonModule, IonContent, BoardComponent ],
 })
 export class CoordinatesPage {
+
+  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  numbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+  subsSeconds: Observable<number> | undefined;
+
+
+  isPlaying = false;
+
+  currentPuzzle = '';
+  puzzles: string[] = [];
+
+  squaresGood: string[] = [];
+  squaresBad: string[] = [];
+  score = 0;
+  time = 60;
+  progressValue = 1;
+  timeColor: 'success' | 'warning' | 'danger' = 'success';
+
+
+    // Options
+    color: 'random' | 'white' | 'black' = 'random';
+    showCoordinates = false;
+    showPieces = false;
+    randomPosition = false;
+    currentFenInBoard = '8/8/8/8/8/8/8/8 w - - 0 1';
+    currentColorInBoard: 'white' | 'black' = 'white';
+  
+    // profile: Profile;
+  
+    private unsubscribeIntervalSeconds$ = new Subject<void>();
+
+  play() {
+    this.puzzles = this.generatePuzzles(200);
+    this.currentPuzzle = this.puzzles[0];
+    this.time = 60;
+    this.score = 0;
+    this.squaresBad = [];
+    this.squaresGood = [];
+
+    let orientation: 'w' | 'b' = this.color === 'white' ? 'w' : 'b';
+
+    if (this.color === 'random') {
+      orientation = Math.random() < 0.5 ? 'w' : 'b';
+    }
+
+    this.currentColorInBoard = orientation === 'w' ? 'white' : 'black';
+    // this.changeOrientation(orientation);
+
+    this.isPlaying = true;
+    this.initInterval();
+  }
+
+  initInterval() {
+
+    const seconds = interval(10);
+    this.subsSeconds = seconds.pipe(
+      takeUntil(this.unsubscribeIntervalSeconds$)
+    );
+
+    this.subsSeconds.subscribe(() => {
+      this.time = this.time - 0.01;
+      this.progressValue = this.time / 60;
+      if (this.time < 1) {
+        // this.stopGame();
+      } else if (this.time > 15) {
+        this.timeColor = 'success';
+      } else {
+        this.timeColor = 'warning';
+      }
+    });
+
+  }
+
+
+
+
+  /**
+   * Generar escaques puzzles
+   *
+   * @count = 1
+   */
+  generatePuzzles(count = 1): string[] {
+    const puzzles = [];
+
+    for (let i = 0; i < count; i++) {
+      // eslint-disable-next-line max-len
+      const puzzle = `${this.letters[Math.floor(Math.random() * this.letters.length)]}${this.numbers[Math.floor(Math.random() * this.numbers.length)]}`;
+      puzzles.push(puzzle);
+    }
+
+    return puzzles;
+  }
+
+
+
+  toggleBoardCoordinates() {
+    this.showCoordinates = !this.showCoordinates;
+    // this.board.destroy();
+    // Eliminar el div vacío
+    const boardContainer = document.getElementById('boardCoordinates');
+    const emptyDiv = Array.from(boardContainer?.children || []).find((child) => child.childElementCount === 0);
+    emptyDiv?.remove();
+
+    // this.loadBoard(this.showCoordinates, this.currentFenInBoard);
+
+  }
+
+
+  toggleShowPieces() {
+    this.showPieces = !this.showPieces;
+    const fenToSet = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'; // Inicial
+
+    if (this.showPieces) {
+      if (this.randomPosition) {
+        // this.currentFenInBoard = randomFEN();
+      } else {
+        this.currentFenInBoard = fenToSet;
+      }
+    } else {
+      this.currentFenInBoard = '8/8/8/8/8/8/8/8 w - - 0 1'; // Vacío
+    }
+
+    // this.board.setPosition(this.currentFenInBoard);
+
+  }
+
+  toggleRandomPosition() {
+    this.randomPosition = !this.randomPosition;
+    if (this.randomPosition) {
+      // this.board.setPosition(randomFEN());
+    } else {
+      // this.board.setPosition('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+    }
+  }
+
+  changeOrientation(orientation?: 'w' | 'b') {
+    // this.board.setOrientation(orientation);
+  }
 
 
 }
