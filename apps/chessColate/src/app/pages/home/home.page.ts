28154c0a4dfa183485725cf8e3e8bc8fdd54c246
Json Alzate <developer.alzate@gmail.com>
2025-09-23 09:03:21 -0500

    🍱 Adicionar assets para los tipos de rutinas tacticas

--- apps/chessColate/src/app/pages/home/home.page.ts
+++ apps/chessColate/src/app/pages/home/home.page.ts
@@ -1,8 +1,7 @@
-// Angular
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // Ionic
-import { IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonLabel, IonCol, IonCard, IonRippleEffect, IonItem, IonImg, IonIcon, IonRow, IonButton, IonNote } from '@ionic/angular/standalone';
 
 // Components
 import { NavbarComponent } from '@shared/components/navbar/navbar.component';
@@ -10,7 +9,7 @@ import { NavbarComponent } from '@shared/components/navbar/navbar.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
+  imports: [IonNote, IonButton, IonRow, IonIcon, IonImg, IonItem, IonRippleEffect, IonCard, IonCol, IonLabel, 
     IonContent,
     CommonModule,
     NavbarComponent
@@ -19,5 +18,24 @@ import { NavbarComponent } from '@shared/components/navbar/navbar.component';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  generalEloPlan5 = 1200;
+  generalEloPlan10 = 1200;
+  generalEloPlan15 = 1200;
+  generalEloPlan20 = 1200;
+  generalEloPlan25 = 1200;
+  generalEloPlan30 = 1200;
   constructor() {}
+
+  planBlood: string = 'warmup'; // 'warmup' | 'backToCalm' | null
+
+  createPlan(type: string) {
+    console.log('Creating plan:', type);
+    // Aquí iría la lógica para crear el plan según el tipo seleccionado
+  }
+
+  goToCustomPlanCreate() {
+    console.log('Navigating to custom plan creation');
+    // Aquí iría la lógica para navegar a la página de creación de planes personalizados
+  }
 }
